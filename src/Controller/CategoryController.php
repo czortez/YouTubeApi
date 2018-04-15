ed019060c1303f22b9de2ff74ab14e2ba35abf20
KrystianBinkowski1984 <krystian.binkowski1984@gmail.com>
2018-04-15 15:31:57 +0200

    add form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,15 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\CategoryEntity;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryController extends Controller
 {
-
-    /**
-     * @Route("/category", name="category")
-     */
     public function showFilms()
     {
         $category = new CategoryEntity();
@@ -29,16 +29,26 @@ class CategoryController extends Controller
             'controller_name' => 'CategoryController',
         ]);
     }
-    public function addCategory()
+    public function addCategory(Request $request)
     {
         $category = new CategoryEntity();
-        $category->setName('DIY search API');
+        $form = $this->createFormBuilder($category)
+            ->add('name', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
 
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($category);
-        $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
 
-        return $this->redirectToRoute('index');
+            return $this->redirectToRoute('categories');
+        }
+        return $this->render('category/new.html.twig', [
+            'controller_name' => 'CategoryController',
+            'form' => $form->createView(),
+            ]);
     }
     public function showCategories()
     {
